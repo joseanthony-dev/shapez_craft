c5ee3bc5547909f2e02204c0bd3bd781026f62b1
JOSE Anthony <a.jose2004.nsi@gmail.com>
2026-03-21 17:00:50 +0100

    Ajout de l'enum outil pour savoir l'outil sélectionné et le bouton actif dans la vue

---
+++ src/modele/jeu/Outil.java
@@ -0,0 +1,8 @@
+package modele.jeu;
+
+/**
+ * Enumération des outils possibles utilisé pour savoir quel outil est sélectionné et quel bouton dans la vue sera actif
+ */
+public enum Outil {
+    TAPIS, MINE, POUBELLE, SUPPRIMER
+}
