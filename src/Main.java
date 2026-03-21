9eb6f578dba4d313f7b0fd3bac08c97dc2bfb5cd
JOSE Anthony <a.jose2004.nsi@gmail.com>
2026-03-21 16:18:58 +0100

    Initialisation du projet avec le code source d'ammorçage

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
+import vuecontroleur.VueControleur;
+import modele.jeu.Jeu;
+
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                Jeu jeu = new Jeu();
+                VueControleur vc = new VueControleur(jeu);
+                jeu.getPlateau().addObserver(vc);
+                vc.setVisible(true);
+            }
+        };
+
+        SwingUtilities.invokeLater(r);
+
     }
+
+
 }
