dabcf980c79585f43512dbe7a746ee8ae3c6e70f
JOSE ANTHONY p2208901 <p2208901@b710pc1410.univ-lyon1.fr>
2026-03-25 15:04:11 +0100

    Ajout des getter et setter du tapis et modification de la fonctin accepteDepuis pour les coins

--- src/modele/plateau/Tapis.java
+++ src/modele/plateau/Tapis.java
@@ -3,8 +3,25 @@ import modele.plateau.Direction;
 import modele.item.ItemShape;
 
 public class Tapis extends Machine{
+    private Direction coinEntree;
     @Override
     public boolean accepteDepuis(Direction provenance) {
-        return provenance == d.direction_opossee();
+        if(coinEntree !=null){
+            return provenance == coinEntree;
+        }else{
+            return provenance == d.direction_opossee();
+        }
     }
+
+    public void setCoinEntree(Direction coinEntree) {
+        this.coinEntree = coinEntree;
+    }
+
+    public Direction getCoinEntree() {
+        return coinEntree;
+    }
+
+    public void detecterCoin(Case c){
+    }
+
 }
