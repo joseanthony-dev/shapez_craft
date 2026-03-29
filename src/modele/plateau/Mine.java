7b85777f89e59df9e7b0ed62ab552d3c2904ee11
JOSE Anthony <a.jose2004.nsi@gmail.com>
2026-03-29 15:15:09 +0200

    ajout d'une condition permettant à la mine de ne pas produire si la mine est déjà pleine évitant de gaspiller de la mémoire

--- src/modele/plateau/Mine.java
+++ src/modele/plateau/Mine.java
@@ -10,13 +10,16 @@ import modele.item.Item;
 public class Mine extends Machine {
     @Override
     public void work() { // TODO : modifier, suivant le gisement fait
-        if (new Random().nextInt(4) == 0) {
-            Item gisement = c.getGisement();
-            // On ajoute en fonction du type de gisement si couleur ou forme coloré
-            if (gisement instanceof ItemShape) {
-                current.add(new ItemShape(((ItemShape) gisement).getCode()));
-            } else if (gisement instanceof ItemColor) {
-                current.add(new ItemColor(((ItemColor) gisement).getColor()));
+        // ajout d'une condition permettant à la mine de ne pas produire si la mine est déjà pleine évitant de gaspiller de la mémoire
+        if(current.isEmpty()){
+            if (new Random().nextInt(4) == 0) {
+                Item gisement = c.getGisement();
+                // On ajoute en fonction du type de gisement si couleur ou forme coloré
+                if (gisement instanceof ItemShape) {
+                    current.add(new ItemShape(((ItemShape) gisement).getCode()));
+                } else if (gisement instanceof ItemColor) {
+                    current.add(new ItemColor(((ItemColor) gisement).getColor()));
+                }
             }
         }
     }
