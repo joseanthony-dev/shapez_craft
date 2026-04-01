db5b3d39c9c210d299036078b48fd5b9fa235de7
JOSE Anthony <a.jose2004.nsi@gmail.com>
2026-04-01 17:45:49 +0200

    Ajout de la JavaDoc

--- src/modele/plateau/Livraison.java
+++ src/modele/plateau/Livraison.java
@@ -1,9 +1,24 @@
+/**
+ * Fichier de classe définissant la zone de livraison
+ * @author JOSE Anthony
+ * @since 2026-03-18
+ * @version 1.0
+ * @see modele.plateau
+ */
 package modele.plateau;
 
+/**
+ * Classe livraison fille de la classe machine
+ */
 public class Livraison extends Machine {
-
+    /**
+     * @serial compteur incrémenté à chaque forme reçu
+     */
     private int compteur = 0;
 
+    /**
+     * Méthode qui incrémente le compteur seulement
+     */
     @Override
     public void work() {
         if (!current.isEmpty()) {
@@ -12,11 +27,18 @@ public class Livraison extends Machine {
         }
     }
 
+    /**
+     * Méthode d'envoi, par défault la zone de livraison n'envoi rien, elle accumulle seulement
+     */
     @Override
     public void send() {
         // La livraison ne renvoie rien
     }
 
+    /**
+     * Fonction qui renvoi la valeur du compteur actuelle
+     * @return la valeur du compteur de la zone de livraison
+     */
     public int getCompteur() {
         return compteur;
     }
--- src/modele/plateau/Machine.java
+++ src/modele/plateau/Machine.java
@@ -1,28 +1,64 @@
+/**
+ * Fichier de classe définissant la zone de livraison
+ * @author JOSE Anthony
+ * @since 2026-03-18
+ * @version 1.0
+ * @see modele.plateau
+ * @see modele.item.Item
+ * @see java.util.LinkedList
+ */
 package modele.plateau;
-
 import modele.item.Item;
 import java.util.LinkedList;
 
+/**
+ * Classe abstraite machine qui implémente runnable
+ */
 public abstract class Machine implements Runnable {
-
+    /**
+     * @serial Liste qui contiendra les items
+     */
     LinkedList<Item> current;
-    Case c;
-    Direction d = Direction.North; // par défaut, pour commencer, tout est orienté au north
 
+    /**
+     * @serial Définit la case de la machine
+     */
+    Case c;
+
+    /**
+     * @serial Définit la direction par défaut vers le nord
+     */
+    Direction d = Direction.North;
+
+    /**
+     * Constructeur permettant d'initialiser la liste contenant les items
+     */
     public Machine()
     {
         current = new LinkedList<Item>();
     }
 
+    /**
+     * Constructeur permettant d'initialiser la liste contenant les items et d'en ajouter un directement
+     * @param _item l'item que l'on veut ajouter à la liste
+     */
     public Machine(Item _item) {
         this();
         current.add(_item);
     }
 
+    /**
+     * Méthode permettant d'initialiser la case avec une case
+     * @param _c la case qui va être affecté à l'attribut
+     */
     public void setCase(Case _c) {
         c= _c;
     }
 
+    /**
+     * Fonction permettant de renvoyer le premier item de la liste
+     * @return l'item de la liste
+     */
     public Item getCurrent() {
         if (current.size() > 0) {
             return current.get(0);
@@ -32,33 +68,37 @@ public abstract class Machine implements Runnable {
     }
 
     /**
-     * Fonction permettant de retourner vraie ou faux selon la provenance de l'objet si on acppete de la faire rentrer dans la machine, on redéfinira les fonctions pour chaque machine
-     * @param provenance la direction depuis laquelle arrive l'objet (la forme)
+     * Fonction permettant de savoir si on accepte ou non l'objet en fonction de la direction de la machine la classe machine elle, accepte tout, la fonction sera redéfini dans chaque sous machines
+     * @param provenance la direction depuis laquelle arrive l'objet
      * @return un booléan si on peut accepter l'objet ou non
      */
     public boolean accepteDepuis(Direction provenance) {
-        return true; // par defaut, une machine accepte de partout
+        return true;
     }
 
-    // On définit la fonction tourner qui fait le changement de direction
+    /**
+     * Méthode permettant de changer la direction et donc de tourner la machine
+     */
     public void tourner() {
         d = d.direction_suivante();
     }
 
     /**
-     * Permet de retourner la direction de la machine, on fera des affichages différents en fonction
+     * Fonction qui permet de retourner la direction de la machine, en fonction de cette direction l'affichage sera changé
      * @return
      */
     public Direction getDirection() {
         return d;
     }
 
-    public void send() // la machine dépose un item sur sa ou ses sorties
+    /**
+     * Méthode permettant d'envoyer l'item actuellement stocké sur une ou plusieurs sorties dans la direction voulu en vérifiant si la machine voisine peut l'accepter suivant son orientation
+     */
+    public void send()
     {
-        Case up = c.plateau.getCase(c, d); // On envoit désormais suivant la direction
+        Case up = c.plateau.getCase(c, d);
         if (up != null) {
             Machine m = up.getMachine();
-            // permet d'envoyer que si la machine est vide, que sa voisine peut accepter et que la file de sa voisine est vide
             if (m != null && !current.isEmpty() && m.accepteDepuis(d.direction_opossee()) && m.current.isEmpty()) {
                 Item item = current.getFirst();
                 m.current.add(item);
@@ -67,12 +107,18 @@ public abstract class Machine implements Runnable {
         }
     }
 
+    /**
+     * Méthode permettant de faire tourner les objets sur eux même à chaque ticks
+     */
     public void work() {
         //if (current.size() > 0 && current.get(0) instanceof ItemShape) {
         //    ((ItemShape) current.get(0)).rotate();
         //}
     }; // action de la machine, aucune par défaut
 
+    /**
+     * Redéfinition de la méthode run qui effectue le travail sur l'item et l'envoi à son prochain
+     */
     @Override
     public void run() {
         work();
